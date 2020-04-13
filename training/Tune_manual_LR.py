4562e4f20641ce2eb7ec5fd660baf6a82fd698bd
Jenna Zhou <rz296@cl.cam.ac.uk>
2020-04-14 00:21:26 +0100

    Update logs for various pipelines

--- training/Tune_manual_LR.py
+++ training/Tune_manual_LR.py
@@ -29,15 +29,15 @@ from sklearn.feature_selection import SelectFromModel
 
 from multiprocessing import Process, Manager
 
-y_train_sampled = np.load('../../datasets/preprocessed/y_train_sampled.npy')
-y_test = np.load('../../datasets/preprocessed/y_test.npy')
-X_train_scaled = np.load('../../datasets/preprocessed/X_train_scaled.npy')
-X_test_scaled = np.load('../../datasets/preprocessed/X_test_scaled.npy')
+y_train_sampled = np.load('../../datasets/preprocessed/npy_files/y_train_sampled.npy')
+y_test = np.load('../../datasets/preprocessed/npy_files/y_test.npy')
+X_train_scaled = np.load('../../datasets/preprocessed/npy_files/X_train_scaled.npy')
+X_test_scaled = np.load('../../datasets/preprocessed/npy_files/X_test_scaled.npy')
 print("Complete loading data")
 #------------------------------------------
-f = open("logs/lr_logs/pca_lr_experiments_log","w")  
+# f = open("logs/lr_logs/pca_lr_experiments_log","w")  
 # f = open("logs/lr_logs/ica_lr_experiments_log","w")  
-# f = open("logs/lr_logs/umap_lr_experiments_log","w")  
+f = open("logs/lr_logs/umap_lr_experiments_log","w")  
 # f = open("logs/lr_logs/fs_lr_experiments_log","w")  
 #--------------------------------------------
 
@@ -47,26 +47,26 @@ C_options = [0.001, 0.01, 0.1, 1, 100, 1000]
 n_components = [30, 40, 50, 60, 70, 80, 90,100]
 
 #-------------------------------------------------------
-# ### PCA
-for n in n_components:
-    pca = PCA(n_components=n)
-    X_train = pca.fit_transform(X_train_scaled)
-    X_test = pca.transform(X_test_scaled)
-    print("Cur n_compo:", n)
-#     print("Total variance of dataset covered:", np.sum(pca.explained_variance_ratio_))
+# # ### PCA
+# for n in n_components:
+#     pca = PCA(n_components=n)
+#     X_train = pca.fit_transform(X_train_scaled)
+#     X_test = pca.transform(X_test_scaled)
+#     print("Cur n_compo:", n)
+# #     print("Total variance of dataset covered:", np.sum(pca.explained_variance_ratio_))
     
 
 #-------------------------------------------------------
 
-### UMAP
-# n_neighbours = [3, 5, 10, 15, 20]
-# min_dist = [0.1, 0.25, 0.4, 0.5]
-# for n in n_components:
-#     for n_nei in n_neighbours:
-#         for d in min_dist:
-#             umap = UMAP(n_neighbors=n_nei, min_dist=d, n_components=n)
-#             X_train = umap.fit_transform(X_train_scaled)
-#             X_test = umap.transform(X_test_scaled)
+## UMAP
+n_neighbours = [3, 5, 10, 15, 20]
+min_dist = [0.1, 0.25, 0.4, 0.5]
+for n in n_components:
+    for n_nei in n_neighbours:
+        for d in min_dist:
+            umap = UMAP(n_neighbors=n_nei, min_dist=d, n_components=n)
+            X_train = umap.fit_transform(X_train_scaled)
+            X_test = umap.transform(X_test_scaled)
 
 #-------------------------------------------------------
 
@@ -75,7 +75,8 @@ for n in n_components:
 #     ica = FastICA(n_components=n)
 #     X_train = ica.fit_transform(X_train_scaled)
 #     X_test = ica.transform(X_test_scaled)
-
+#------------------------------------------------
+    
 # # L1 Regularisation
 # alpha=[0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.15, 0.2]
 # for a in alpha:
@@ -92,75 +93,76 @@ for n in n_components:
 #     print(grid.cv_results_['params'])
 #     print(mean_scores)
 #     print(grid.best_params_)
-    cm_tp=[[0,0],[0,0]]
-    for penalty in ["l1","l2"]:
-        param_grid = [
-            {
-                'C': C_options,
-            },
-        ]
+            cm_tp=[[0,0],[0,0]]
+            for penalty in ["l1","l2"]:
+                param_grid = [
+                    {
+                        'C': C_options,
+                    },
+                ]
 
-        print("Shape of X train:", X_train.shape)
-        print("Shape of y train:", y_train_sampled.shape)
+                print("Shape of X train:", X_train.shape)
+                print("Shape of y train:", y_train_sampled.shape)
 
-        grid = GridSearchCV(LogisticRegression(max_iter=1000, penalty=penalty, solver='saga'), param_grid=param_grid, scoring="precision", cv=StratifiedKFold(n_splits=3, shuffle=True, random_state=42), n_jobs=-1, verbose=1)
-        grid.fit(X_train, y_train_sampled)
-    #     print("With Lasso(L2) FS alpha=",a,"and l2, the best params are:"
+                grid = GridSearchCV(LogisticRegression(max_iter=1000, penalty=penalty, solver='saga'), param_grid=param_grid, scoring="precision", cv=StratifiedKFold(n_splits=3, shuffle=True, random_state=42), n_jobs=-1, verbose=1)
+                grid.fit(X_train, y_train_sampled)
+            #     print("With Lasso(L2) FS alpha=",a,"and l2, the best params are:"
 
-        cur_params = {
-    #         "n_neighbour": n_nei,
-    #         "min_dist": d,
-            "n_component":n,
-            "C":grid.best_params_["C"],
-            "penalty":penalty
+                cur_params = {
+                    "n_neighbour": n_nei,
+                    "min_dist": d,
+                    "n_component":n,
+                    "C":grid.best_params_["C"],
+                    "penalty":penalty
 
-        }
-        f.write(str(cur_params))
-        f.write("\n")
-        
-        
-        #Use Testing set to check for overfitting
-        clf = LogisticRegression(max_iter=1000, C=grid.best_params_["C"], penalty=penalty, solver='saga')
-        y_pred_tr = clf.predict(X_train)
-        y_pred_te = clf.predict(X_test)
-        cm_tr = confusion_matrix(y_train_sampled, y_pred_tr)
-        cm_te = confusion_matrix(y_test, y_pred_te)
-    #             print("Confusion matrix of PPMI training set:")
-    #             print(cm_tr)
-    #             print("Confusion matrix of PPMI testing set:")
-    #             print(cm_te)
+                }
+                f.write(str(cur_params))
+                f.write("\n")
 
 
-        if cm_te[0][0] >= 10:
-            f.write("Confusion matrix of PPMI training set:\n")
-            f.write(str(cm_tr))
-            f.write("\n")
-            f.write("Confusion matrix of PPMI testing set:\n")
-            f.write(str(cm_te))
-            f.write("\n")
-            f.write("precision of testing set:" + str(precision_score(y_test, y_pred_te)))
-            f.write("\n")
-
-        if cm_te[0][0] > cm_tp[0][0]:
-            cm_tp = cm_te
-            params_flag = cur_params
-            f.write("The temp confmatx of testing set has been updated to:\n")
-            f.write(str(cm_tp)+"\n")
-            f.write("\n")
-        elif cm_te[0][0] == cm_tp[0][0] and cm_te[1][1] > cm_tp[1][1]:
-            cm_tp = cm_te
-            params_flag = cur_params
-            f.write("The temp confmatx of testing set has been updated to:\n")
-            f.write(str(cm_tp))
-            f.write("\n")
+                #Use Testing set to check for overfitting
+                clf = LogisticRegression(max_iter=1000, C=grid.best_params_["C"], penalty=penalty, solver='saga')
+                clf.fit(X_train, y_train_sampled)
+                y_pred_tr = clf.predict(X_train)
+                y_pred_te = clf.predict(X_test)
+                cm_tr = confusion_matrix(y_train_sampled, y_pred_tr)
+                cm_te = confusion_matrix(y_test, y_pred_te)
+            #             print("Confusion matrix of PPMI training set:")
+            #             print(cm_tr)
+            #             print("Confusion matrix of PPMI testing set:")
+            #             print(cm_te)
 
 
-    f.write("For PCA n_compo="+str(n)+",from confusion matrix of PPMI testing set, best params are: \n")
+                if cm_te[0][0] >= 10:
+                    f.write("Confusion matrix of PPMI training set:\n")
+                    f.write(str(cm_tr))
+                    f.write("\n")
+                    f.write("Confusion matrix of PPMI testing set:\n")
+                    f.write(str(cm_te))
+                    f.write("\n")
+                    f.write("precision of testing set:" + str(precision_score(y_test, y_pred_te)))
+                    f.write("\n")
+
+                if cm_te[0][0] > cm_tp[0][0]:
+                    cm_tp = cm_te
+                    params_flag = cur_params
+                    f.write("The temp confmatx of testing set has been updated to:\n")
+                    f.write(str(cm_tp)+"\n")
+                    f.write("\n")
+                elif cm_te[0][0] == cm_tp[0][0] and cm_te[1][1] > cm_tp[1][1]:
+                    cm_tp = cm_te
+                    params_flag = cur_params
+                    f.write("The temp confmatx of testing set has been updated to:\n")
+                    f.write(str(cm_tp))
+                    f.write("\n")
+
+
+    f.write("For UMAP n_compo="+str(n)+",from confusion matrix of PPMI testing set, best params are: \n")
 #     print("For FS alpha=",a,",from confusion matrix of PPMI testing set, best params are:")
     f.write(str(params_flag))
     f.write("\n")
     f.write(str(cm_tp))
-    f.write("\n")
+    f.write("\n\n")
     print("Complete experiments for ", n, "components")
 
     
